fe4a3bf12c3ac88f0a0e7fd249d0b76e0e306db3
Mark <mark.kenny@yandex.ru>
2022-05-19 18:41:11 +0300

    Split the code into separate functions.

--- handlers/client.py
+++ handlers/client.py
@@ -7,6 +7,10 @@ from youtubesearchpython import VideosSearch, VideoDurationFilter, CustomSearch
 # todo: youtubesearchpython.__future__
 import os, sys
 
+# Импортируем модуль datetime (определяем переменные для записи в БД)
+import datetime
+from data_base.sqlite_db import sql_start
+
 # import logging
 # logging.basicConfig(
 #     handlers=[
@@ -67,33 +71,67 @@ async def start_command(message: types.Message):
             keyboard.row(InlineKeyboardButton(i[0], callback_data=i[1]))
     await message.answer('Выберите название трека', reply_markup=keyboard)
 
-    #  отлавливаем нажатие пользователя на инлайнкнопку --> возвращается ID трека
-    @dp.callback_query_handler()
-    async def callback_func(query):
-        data = query.data
-        # метод достаёт всю информацию о 1-ом видео (по ID (data) )
-        videosSearch_on_id = VideosSearch(data, limit=1)
-        song = videosSearch_on_id.resultComponents[0]
 
-        # создаем список, если id песни есть в директории или пустой список - если такого id нет
+# Определяем функцию, которая возвращает нам имя скаченного файла
+async def get_filename(query, song):
+    """
+    Определяем функцию, которая возвращает нам имя скаченного файла
+    :param query:
+    :param song:
+    :return:
+    """
+    # создаем список, если id песни есть в директории или пустой список - если такого id нет
+    filename_find = [i for i in os.listdir('downloads_music') if song['id'] in i]
+    if not filename_find:
+        await query.answer('Прошу немного подождать, идёт загрузка трека')
+        # функция, при помощи которой мы скачиваем видео из YouTube и переводим в Mp3 формат
+        download(song['id'], query)
+
         filename_find = [i for i in os.listdir('downloads_music') if song['id'] in i]
+    await query.answer('Скоро пришлем трек')
+    filename = filename_find[0]
+    return filename
 
-        try:
-            if not filename_find:
-                await query.answer('Прошу немного подождать, идёт загрузка трека')
-                # функция, при помощи которой мы скачиваем видео из YouTube и переводим в Mp3 формат
-                download(song['id'], query)
 
-            filename_find = [i for i in os.listdir('downloads_music') if song['id'] in i]
-            filename = filename_find[0]
-            # открытие файла типа open(), и InputFile запихивает указатель на этот файл (указываем путь к файлу)
-            file_audio_send = aiogram.types.input_file.InputFile(rf'C:\Users\Admin\PycharmProjects\MusicTelegramBot\downloads_music\{filename}')
+#  функция, которая берет имя пользователя, название песни и сегодняшнюю дату и записывает в БД
+def insert_music_data_in_db(query, song):
+    users_name = query.from_user.first_name
+    music_name = song['title']
+    download_date = datetime.datetime.today()
 
-            await query.answer('Скоро пришлём трек!')
-            await bot.send_audio(query.from_user.id, file_audio_send)
+    sql_start(users_name, music_name, download_date)
 
-            # logging.info(f'SEND_AUDIO {query.from_user.id}, {file_audio_send}', exc_info=1)
-        except:
-            await message.answer('Произошла ошибка при загрузке трека')
+
+#  функция возвращает данные песни (имя, время, ссылку и тд...), которую выбрал пользователь
+def get_song(query):
+    data = query.data
+    # метод достаёт всю информацию о 1-ом видео (по ID (data) )
+    videosSearch_on_id = VideosSearch(data, limit=1)
+    song = videosSearch_on_id.resultComponents[0]
+    return song
+
+
+#  получаем путь к запрошенной аудиозаписи
+async def get_file_audio(query):
+    song = get_song(query)
+    filename = await get_filename(query, song)  # определяем переменную filename
+    insert_music_data_in_db(query, song)
+
+    # try:
+    # открытие файла типа open(), и InputFile запихивает указатель на этот файл (указываем путь к файлу)
+    file_audio_send = aiogram.types.input_file.InputFile(rf'C:\Users\Admin\PycharmProjects\MusicTelegramBot\downloads_music\{filename}')
+    # except:
+    #     await query.answer('Произошла ошибка при загрузке трека')
+    return file_audio_send
+
+
+#  отлавливаем нажатие пользователя на инлайнкнопку --> возвращается ID трека
+@dp.callback_query_handler()
+async def callback_func(query):
+    file_audio_send = await get_file_audio(query)
+    await bot.send_audio(query.from_user.id, file_audio_send)
+
+    # logging.info(f'SEND_AUDIO {query.from_user.id}, {file_audio_send}', exc_info=1)
+
 
 
