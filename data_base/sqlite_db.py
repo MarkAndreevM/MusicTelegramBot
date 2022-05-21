49be052b396e95250b40da9938140e8e5ce99bfe
Mark <mark.kenny@yandex.ru>
2022-05-21 15:01:30 +0300

    some changes

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -8,11 +8,6 @@ database_path = 'users_who_have_downloaded_music.db'
 def sql_start():
     connection = sqlite3.connect(database_path)
 
-    if connection:
-        print('Data connection connected OK!')
-    else:
-        raise Exception('Ошибка подключения к базе данных')
-
     cur = connection.cursor()
 
     cur.execute("CREATE TABLE IF NOT EXISTS users_download_music (users_name TEXT, music_name TEXT, download_date TEXT)")
@@ -23,15 +18,9 @@ def sql_start():
 def sql_insert(users_name, music_name, download_date):
     connection = sqlite3.connect(database_path)
 
-    if connection:
-        print('Data connection connected OK!')
-    else:
-        raise Exception('Ошибка подключения к базе данных')
-
     cur = connection.cursor()
 
     cur.execute("INSERT INTO users_download_music VALUES (?,?,?)", (users_name, str(music_name), str(download_date)))
     connection.commit()
     connection.close()
-
 
